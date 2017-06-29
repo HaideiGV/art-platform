d293bb08596e4a59f05be860a2a9d9a4e1188426
Viacheslav Haidei <viacheslav.haidei@nextiva.com>
2017-06-29 23:12:24 +0300

    add filter

--- web/controllers/master_controller.ex
+++ web/controllers/master_controller.ex
@@ -6,7 +6,7 @@ defmodule ArtPlatform.MasterController do
     masters = Repo.all(Master)
     all_masters = from m in Master, distinct: :city, select: m.city
     cities = Repo.all(all_masters)
-    render(conn, "index.html", masters: masters, cities: nil)
+    render(conn, "index.html", masters: masters, cities: cities)
   end
 
   def new(conn, _params) do
